df3ad5bc906bda3b3f99ede52da11ea2fcf0ff0d
Alexander Zubkov <info@zubkov.info>
2016-08-30 16:34:15 +0300

    Add tasks to different queues

--- app/models/concerns/create_chef_task.rb
+++ app/models/concerns/create_chef_task.rb
@@ -2,6 +2,19 @@ module CreateChefTask
   extend ActiveSupport::Concern
 
   def create_chef_task(action)
-    $redis.lpush('tasks', to_chef_json(action))
+    json = to_chef_json(action)
+    if [SystemUser, SystemGroup, Apache, Vhost].include?(self.class)
+      ApacheVariation.all.each do |av|
+        add_task_to_redis(json, av.name)
+      end
+    else
+      add_task_to_redis(json)
+    end
+  end
+
+  protected
+  def add_task_to_redis(json, queue = nil)
+    queue = 'root' if queue.nil?
+    $redis.lpush("tasks_for_#{queue}", json)
   end
 end
