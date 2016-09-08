b16c99e76c217914c60376ba4c01bfbb3cb31894
Alexander Zubkov <info@zubkov.info>
2016-09-08 10:32:55 +0300

    Create different JSON for different apache variations

--- app/models/concerns/create_chef_task.rb
+++ app/models/concerns/create_chef_task.rb
@@ -2,13 +2,12 @@ module CreateChefTask
   extend ActiveSupport::Concern
 
   def create_chef_task(action)
-    json = to_chef_json(action)
     if [SystemUser, SystemGroup, Apache, Vhost].include?(self.class)
       ApacheVariation.all.each do |av|
-        run_chef_client(json, av.name)
+        run_chef_client(to_chef_json(action, av), av.name)
       end
     else
-      run_chef_client(json)
+      run_chef_client(to_chef_json(action))
     end
   end
 
